2a6bf89cc2e33b12edcf57f880a0dd7ac8ae8534
Rose Robrecht <annikarobrecht@mail.weber.edu>
2019-07-15 08:45:35 -0600

    .

--- guessScript.js
+++ guessScript.js
@@ -3,4 +3,19 @@ var randNum = Math.round( Math.random() * 99 ) + 1;
 function sendData(form) {
     var guessNum = form.inputbox.value;
     form.inputbox.value = ""; 
+
+    if (guessNum==randNum) {
+        console.log("You got it!");
+    }
+
+    while(guessNum!=randNum) {
+        console.log("Sorry, that's not the answer");
+        if (guessNum > randNum) {
+            console.log("You need to guess lower");
+        }
+        else {
+            console.log("You need to guess lower");
+        }
+        guessNum = form.inputbox.value;
+    }
 }
