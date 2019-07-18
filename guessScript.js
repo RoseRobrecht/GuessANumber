236e2f56a127ddfc86a3949d7bfb870e3cfbfe9a
Rose Robrecht <annikarobrecht@mail.weber.edu>
2019-07-17 19:03:24 -0600

    did stuff

--- guessScript.js
+++ guessScript.js
@@ -2,6 +2,8 @@ var randNum = Math.round( Math.random() * 99 ) + 1;
 console.log(randNum);
 
 sendData = (form) => {
+    var guessCount = 1
+
     var guessNum = parseInt(form.inputbox.value, 10);
     form.inputbox.value = ""; 
     
@@ -9,11 +11,11 @@ sendData = (form) => {
     document.getElementById("response").innerHTML = text
 
     if (guessNum===randNum) {
-        var rightOrWrong = "You got it! Good job.";
+        var rightOrWrong = "You got it! Good job. <br> You guessed a total of " + guessCount + " times!";
         highLow = "";
         document.getElementsByTagName('body')[0].className = 'winner'
     }
-    else {
+    else if ((guessNum >= 1) && (guessNum <= 100)) {
         rightOrWrong = "Sorry, that's not the answer";
         if (guessNum > randNum) {
             var highLow  = "That was a little high, you need to guess lower";
@@ -22,7 +24,13 @@ sendData = (form) => {
             highLow  = "That was a little low, you need to guess higher";
         }
         guessNum = form.inputbox.value;
+        guessCount += 1
     }
+    else {
+        rightOrWrong = "The number you guessed is out of range."
+        highLow = "Please guess a number between 1 and 100."
+    }
+    
 
     document.getElementById("right-or-wrong").innerText = rightOrWrong;
     document.getElementById("high-or-low").innerText = highLow;
